a98eda4f2cd0033d0fd9de7c4d5a3ab7fb8a1c2f
JavaMan <Kechin.d@gmail.com>
2024-12-29 17:00:30 +0300

    init

--- src/main/java/ci_cd_github_actions/example/ExampleController.java
+++ src/main/java/ci_cd_github_actions/example/ExampleController.java
@@ -5,14 +5,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("/api/divider")
+@RestController // объявляем компонент rest controller
+@RequestMapping("/api/divider") // настраиваем корень эндпоинта
 public class ExampleController {
 
-    @GetMapping
+    @GetMapping // декларпируем get метод счета.
     public String divide(@RequestParam Double divisible, @RequestParam Double divider) {
-        if (divisible == null || divider == null || divider == 0) return "NaN";
-        return "" + divisible / divider;
+        if (divisible == null || divider == null || divider == 0) return "NaN"; // проверка краевых случаев
+        return String.valueOf(divisible / divider); // привидение к строке
     }
 
 }
